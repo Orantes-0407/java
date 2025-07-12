cf284e9de18bd372936c11909b30bcd4233db58d
Melany Rosmery Pérez Orantes <mperezo37@miumg.edu.gt>
2025-07-12 10:43:53 -0600

    Add files via upload

---
+++ Main2.java
@@ -0,0 +1,23 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // Press Alt+Intro with your caret at the highlighted text to see how
+        // IntelliJ IDEA suggests fixing it.
+        System.out.printf("Hello and welcome!\t");
+        System.out.printf("Mi primer programa.");
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Ingrese el primer número (a);");
+        int a = scanner.nextInt();
+        System.out.print("Ingrese el segundo número (b);");
+        int b = scanner.nextInt();
+        //calcular la operación
+        int resultado = a+b;
+        System.out.print("El resultado es:"+ resultado);
+        scanner.close();
+
+
+
+    }
+}
---
+++ Main4.java
@@ -0,0 +1,20 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // Press Alt+Intro with your caret at the highlighted text to see how
+        // IntelliJ IDEA suggests fixing it.
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Ingrese un número entero positivo: ");
+        int numero = scanner.nextInt();
+        long factorial = 1;
+
+        for (int i = 1; i <= numero; i++) {
+            factorial *= i;
+        }
+
+        System.out.println("El factorial de " + numero + " es: " + factorial);
+
+}
+    }
